20a1d65831bfc35a853d0cb72725b09116d2cb7e
Samanta Adriane Sedlaczek Luchoski <49082205+samantaluc@users.noreply.github.com>
2023-02-15 18:36:39 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,16 +7,10 @@ const Body = Matter.Body;
 var btn2;
 var angle=60; // variavel do angulo inicial
 function setup() {
-  createCanvas(400,400);
-  engine = Engine.create();  world = engine.world;
-   var ball_options = {
-    restitution: 0.95,    frictionAir:0.01
-  }
-   var ground_options ={     isStatic: true
-   };
-   btn2 = createImg('up.png');  btn2.position(350,30);  btn2.size(50,50);  btn2.mouseClicked(vForce);  ground1 = Bodies.rectangle(100,300,100,20,ground_options);
-  World.add(world,ground1);  ball = Bodies.circle(100,10,20,ball_options);  World.add(world,ball);  ground = Bodies.rectangle(100,400,650,20,ground_options);
-  World.add(world,ground);  rectMode(CENTER);  ellipseMode(RADIUS);
+  createCanvas(400,400);  engine = Engine.create();  world = engine.world;
+   var ball_options = {    restitution: 0.95,    frictionAir:0.01  }
+   var ground_options ={     isStatic: true   };
+   btn2 = createImg('up.png');  btn2.position(350,30);  btn2.size(50,50);  btn2.mouseClicked(vForce);  ground1 = Bodies.rectangle(100,300,100,20,ground_options);  World.add(world,ground1);  ball = Bodies.circle(100,10,20,ball_options);  World.add(world,ball);  ground = Bodies.rectangle(100,400,650,20,ground_options);  World.add(world,ground);  rectMode(CENTER);  ellipseMode(RADIUS);
 }
 function draw() {
   background(51);
